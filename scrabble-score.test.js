929048820c1a3295a8304b76d7df912be409fcff
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-08-06 13:31:21 +0200

    started task 2

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,7 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+
+import { expect, test } from "vitest";
+
+test('scrabble-score test', () => {
+    expect(calculateScrabbleScore ("A")).toBe(1)
+  })
